09248dc41e0f942ef7d87b005086e614e1eb4d66
IdkowiakFabrice <idkowiak.fabrice@gmail.com>
2021-02-26 10:38:30 +0100

    fonction qui retourne 3 tableaux un avec les plats, un avec les tailles, et le derniers avec les quantités

--- helpers/command.js
+++ helpers/command.js
@@ -15,7 +15,38 @@ const userCommand = () => {
 
         rl.close();
     });
-    return answerU;
+    // return answerU;
+    
+    if(answerU.indexOf(';') !== 0) {
+        let splittedAnswerU = answerU.split(';')
+        let nbCommands = splittedAnswerU.length
+        let dishs = []
+        let sizes = []
+        let count = []
+        for(var i = 0; i < nbCommands; i ++) {
+            if(splittedAnswerU[i] === '') {
+                splittedAnswerU.splice(i,1)
+            }
+            var currentCommand = splittedAnswerU[i].split(' ')
+            if(currentCommand[0] === '') {
+                currentCommand.splice(0,1)
+            }
+            console.log(currentCommand)
+            if(currentCommand.length === 3) {
+                dishs.push(currentCommand[0])
+                sizes.push(currentCommand[1])
+                var countNumber = currentCommand[2].replace('x','')
+                count.push(countNumber)
+                console.log(`Validé pour la commande ${i+1} contenant : ${countNumber} x ${currentCommand[0]} de taille ${currentCommand[1]}`)
+            
+            } else {
+                console.log(`Error: your command ${i+1} need a name, a size and a count`)
+            }
+        }
+        return [dishs, sizes, count]
+    } else {
+        console.log('You answer need a ; at the end or between each dish')
+    }
 }
 
 export default {
